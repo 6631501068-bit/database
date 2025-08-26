16be36dabf279fe9a345830ecba8115c8c08f3c0
Broccoli <6613501068@lamduan.mfu.ac.th>
2025-08-27 02:53:22 +0700

    main commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const con = require('./db');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+
+// password generator
+app.get('/password/:pass', (req, res) => {
+    const password = req.params.pass;
+    bcrypt.hash(password, 10, function(err, hash) {
+        if(err) {
+            return res.status(500).send('Hashing error');
+        }
+        res.send(hash);
+    });
+});
+
+
+// login
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    const sql = "SELECT id, password FROM users WHERE username = ?";
+    con.query(sql, [username], function(err, results) {
+        if(err) {
+            return res.status(500).send("Database server error"+ err.message);
+        }
+        if(results.length != 1) {
+            return res.status(401).send("Wrong username");
+        }
+        // compare passwords
+        bcrypt.compare(password, results[0].password, function(err, same) {
+            if(err) {
+                return res.status(500).send("Hashing error");   
+            }
+            if(same) {
+                return res.send("Login OK");
+            }
+            return res.status(401).send("Wrong password");
+        });
+    })
+});
+
+app.get("/Expense",(req,res)=>{
+
+})
+
+app.get("/ExpenseToday",(req,res)=>{
+    
+})
+
+app.get("/SearchExpense",(req,res)=>{
+    
+})
+
+app.post("/AddnewExpense",(req,res)=>{
+    
+})
+
+app.delete("/DeleteAnExpense",(req,res)=>{
+    
+})
+
+// ---------- Server starts here ---------
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('Server is running at ' + PORT);
+});
+
